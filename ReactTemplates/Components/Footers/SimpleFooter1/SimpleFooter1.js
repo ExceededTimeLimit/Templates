d0a251805a32731563a60491738ce4eb6ec74512
p-m986 <77034913+p-m986@users.noreply.github.com>
2023-02-16 11:25:13 +0530

    Added the SimpleFooter1

---
+++ ReactTemplates/Components/Footers/SimpleFooter1/SimpleFooter1.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./SimpleFooter1.scss"
+
+const SimpleFooter1 = () =>{
+    return(
+        <div className="Footer">
+            <p className="Footer__text">&#169; Year, Company name</p>
+        </div>
+    );
+}
+
+export default SimpleFooter1;
